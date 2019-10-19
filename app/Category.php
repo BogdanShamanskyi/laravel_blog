545c3ddf2d7a6d0a81ea13142badec03b61320f9
Shamanskyi <you@example.com>
2019-10-19 19:38:19 +0300

    Created models

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = [
+    	'name', 'description'
+    ];
+
+    public function comments() {
+    	return $this->morphMany(Comment::class, 'commentable');
+    }
+
+    public function posts() {
+    	return $this->morphedByMany(Post::class, 'categoryable');
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+    	'name', 'content', 'file'
+    ];
+
+	public function categories() {
+    	return $this->morphToMany(Category::class, 'categoryable');
+    }
+
+    public function comments() {
+    	return $this->morphMany(Comment::class, 'commentable');
+    }
 }
