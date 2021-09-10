31e4893d8fe2cec443918e374bcc39937794e7db
BogdanShamanskyi <bogdanshamanskyi@gmail.com>
2021-09-10 23:21:26 +0300

    Added hasMany posts to comment

--- app/Comment.php
+++ app/Comment.php
@@ -3,10 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Comment extends Model
 {
     protected $fillable = [
     	'author', 'content', 'post_id'
     ];
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
 }
