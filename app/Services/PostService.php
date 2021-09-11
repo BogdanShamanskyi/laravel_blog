4efa3a77a9b56a025de201a203ca576840c60113
BogdanShamanskyi <bogdanshamanskyi@gmail.com>
2021-09-11 21:02:21 +0300

    Created method update for PostService

--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Http\Requests\UpdatePostRequest;
 use App\Post;
 
 class PostService
@@ -15,6 +16,30 @@ class PostService
         $post->save();
 
         $post->categories()->attach($postData['categories']);
+
+        return $post;
+    }
+
+    public function update(UpdatePostRequest $request, Post $post): Post
+    {
+        $post->update($request->except('file'));
+
+        if(isset($request->check_delete_old)) {
+            File::delete(public_path('PostsFiles').'/'.$post->file);
+            $post->file = null;
+            $post->save();
+        }
+
+        if($request->hasFile('file')) {
+            $file = $request->file('file');
+            $file_name = rand() . '.' . $file->getClientOriginalExtension();
+            $file->move(public_path('PostsFiles'), $file_name);
+            $post->file = $file_name;
+            $post->save();
+        }
+
+        $post->categories()->detach();
+        $post->categories()->attach($request->input('categories'));
 
         return $post;
     }
