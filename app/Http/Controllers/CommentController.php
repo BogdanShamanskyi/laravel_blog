77552c407bc1cd3a1be9b29de0251eb00534849d
BogdanShamanskyi <bogdanshamanskyi@gmail.com>
2021-09-14 23:41:48 +0300

    Changed code style in CommentController

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,22 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use App\Comment;
 
 class CommentController extends Controller
 {
-    public function getComments($commentable_type, $commentable_id) 
+    public function getComments($commentable_type, $commentable_id): Collection
     {
-    	$comments = Comment::where('commentable_id', $commentable_id);
+    	$comments = Comment::query()->where('commentable_id', $commentable_id);
     	$comments = $comments->where('commentable_type', $commentable_type)->get();
+
     	foreach($comments as $comment) {
             $comment->date = $comment->created_at->diffForHumans();
         }
+
     	return $comments;
     }
 
-    public function postComment($type, $id, Request $request) 
+    public function postComment($type, $id, Request $request): Comment
     {
     	$this->validate($request, [
     		'author' => 'required|by_author',
@@ -25,12 +28,13 @@ class CommentController extends Controller
     	]);
 
     	$comment = new Comment();
-    	$comment->author = $request->author;
-    	$comment->content = $request->content;
+    	$comment->author = $request->get('author');
+    	$comment->content = $request->get('content');
     	$comment->commentable_id = $id;
     	$comment->commentable_type = $type;
     	$comment->save();
     	$comment->date = $comment->created_at->diffForHumans();
+
     	return $comment;
     }
 }
