eaeb80522e5d53d32081b654f5984c6799033b32
BogdanShamanskyi <bogdanshamanskyi@gmail.com>
2021-09-16 23:59:04 +0300

    Added getPostComments method to CommentController

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\PostCommentRequest;
 use App\Services\CommentService;
 use Illuminate\Database\Eloquent\Collection;
 use App\Comment;
+
 
 class CommentController extends Controller
 {
@@ -19,23 +19,14 @@ class CommentController extends Controller
         $this->commentService = $commentService;
     }
 
-    public function getComments($commentable_type, $commentable_id): Collection
+    public function getPostComments(int $id): Collection
     {
-    	$comments = Comment::query()->where('commentable_id', $commentable_id);
-    	$comments = $comments->where('commentable_type', $commentable_type)->get();
+        $comments = Comment::query()->where('post_id', $id)->get();
 
-    	foreach($comments as $comment) {
+        foreach($comments as $comment) {
             $comment->date = $comment->created_at->diffForHumans();
         }
 
-    	return $comments;
-    }
-
-    public function postComment(PostCommentRequest $request, $type, $id): Comment
-    {
-        $comment = $this->commentService->create($request->commentData($type, $id));
-    	$comment->date = $comment->created_at->diffForHumans();
-
-    	return $comment;
+        return $comments;
     }
 }
