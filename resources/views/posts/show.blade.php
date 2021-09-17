2cfbf2138cab0cce2226145486387f7c7311c897
BogdanShamanskyi <bogdanshamanskyi@gmail.com>
2021-09-17 23:09:15 +0300

    Remove comments-ajax.js script

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,11 +3,7 @@
 @section('content')
 	@include('layouts.sidebar')
 	<div class="col-md-8 blog-main">
-		
 		@include('posts.partials.post')
-
 		@include('layouts.comments')
-		
-		<script src="{{ asset('js/comments-ajax.js') }}"></script>
 	</div>
 @endsection
