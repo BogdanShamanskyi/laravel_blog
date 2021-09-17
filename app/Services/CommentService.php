53e8d4f5b00e9b48fb94956c423ac35ab9020c30
BogdanShamanskyi <bogdanshamanskyi@gmail.com>
2021-09-17 23:03:14 +0300

    Added getPostComments method to CommentService.php

--- app/Services/CommentService.php
+++ app/Services/CommentService.php
@@ -3,11 +3,23 @@
 namespace App\Services;
 
 use App\Comment;
+use Illuminate\Database\Eloquent\Collection;
 
 class CommentService
 {
 	public function create(array $data): Comment
     {
         return Comment::create($data);
+    }
+
+    public function getPostComments(int $id): Collection
+    {
+        $comments = Comment::query()->where('post_id', $id)->get();
+
+        foreach($comments as $comment) {
+            $comment->date = $comment->created_at->diffForHumans();
+        }
+
+        return $comments;
     }
 }
