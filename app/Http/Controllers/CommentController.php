7020b7cc23646928fa180f0edde5d2da392890dc
BogdanShamanskyi <bogdanshamanskyi@gmail.com>
2021-09-19 00:21:55 +0300

    Refactored CommentController.php

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PostCommentRequest;
 use App\Services\CommentService;
 use Illuminate\Database\Eloquent\Collection;
+use App\Comment;
 
 class CommentController extends Controller
 {
@@ -17,8 +19,13 @@ class CommentController extends Controller
         $this->commentService = $commentService;
     }
 
-    public function getPostComments(int $id): Collection
+    public function getComments(string $type, int $id): Collection
     {
-        return $this->commentService->getPostComments($id);
+        return $this->commentService->get($id, $type);
+    }
+
+    public function postComment(PostCommentRequest $request, string $type, int $id): Comment
+    {
+        return $this->commentService->make($request->commentData($id, $type));
     }
 }
