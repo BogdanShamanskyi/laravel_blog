c917f79063aa70746da918780146f4f5ca9533e9
BogdanShamanskyi <bogdanshamanskyi@gmail.com>
2021-09-19 00:21:15 +0300

    Added make method to CommentService.php

--- app/Services/CommentService.php
+++ app/Services/CommentService.php
@@ -7,9 +7,12 @@ use Illuminate\Database\Eloquent\Collection;
 
 class CommentService
 {
-	public function create(array $data): Comment
+    public function make(array $data): Comment
     {
-        return Comment::create($data);
+        $comment = Comment::create($data);
+        $comment->date = $comment->created_at->diffForHumans();
+
+        return $comment;
     }
 
     public function get(int $id, string $type): Collection
